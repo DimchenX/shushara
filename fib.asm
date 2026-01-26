ce4a3232578ac475b0220647e2b27d91d9f2652a
Dmitriy <34652935+DimchenX@users.noreply.github.com>
2026-01-26 07:37:50 +0300

    Add files via upload

--- fib.asm
+++ fib.asm
@@ -1,18 +1,18 @@
 format PE64 console
+stack 20000h
+entry start
 
 include 'win64a.inc'
 
-sub rsp, 20h
+section '.text' code executable readable writeable
 
+start:
 push 46
 call fib
-
 push rax
-invoke prin, msg, rax
+cinvoke prin, msg, rax
+;cinvoke prin, msg+6, qword[cou_]
 pop rax
-
-
-add rsp, 20h
 ret
 
 fib:
@@ -32,16 +32,18 @@ pop rcx
 add rcx, rax
 .end:
 mov rax, rcx
+;inc qword[cou_]
 ret 8
-
-msg db 0ah, 'fib = %d', 0ah, 0ah, 0
 
 align 10h
 
 data import
-
-library msvcrt, 'msvcrt.dll'
-
-import msvcrt, prin, 'printf'
-
+library  msvcrt, "msvcrt.dll"
+import   msvcrt,  prin, "printf"
 end data
+
+align 10h
+
+;cou_  dq 0
+msg db "fib = %d", 0Ah, 0Ah, 0
+
