95fe27e391a270da07c5ff629c20e6aebf1c61e9
Dmitriy <34652935+DimchenX@users.noreply.github.com>
2026-01-28 21:21:21 +0300

    Add files via upload

--- fib.java
+++ fib.java
@@ -1,16 +1,14 @@
 public class fib {
 
-    public static int fib(int i){
+public static long cnt = 0;
 
-         if (i < 2) return i;
-         return fib(i - 2) + fib(i - 1);
+public static long fib(long i) {
+ cnt ++;
+ if(i < 2) return i;
+ return fib(i - 2) + fib(i - 1);
+}
 
-    }
-
-    public static void main (String[] args) {
-
-        System.out.println(fib(46));
-
-    }
-
+public static void main (String[] args) {
+ System.out.println("fib = " + fib(46) + "\ncounts = " + cnt + "\n");
+}
 }
